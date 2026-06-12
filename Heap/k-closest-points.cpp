bba664108d671f97685a0934ecb9f506a52dcded
Sanskriti Raj Rathor <159160558+Sanskritiraj02@users.noreply.github.com>
2026-06-12 22:33:19 +0530

    Add kClosest function to find closest points
    Implemented a solution to find the k closest points to the origin using a max heap.

---
+++ Heap/k-closest-points.cpp
@@ -0,0 +1,32 @@
+```cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // Max Heap: store {distance, point}
+        priority_queue<pair<int, vector<int>>> maxHeap;
+
+        for (auto &p : points) {
+            int x = p[0];
+            int y = p[1];
+            int dist = x*x + y*y; // no need sqrt
+
+            maxHeap.push({dist, p});
+
+            if (maxHeap.size() > k) {
+                maxHeap.pop();
+            }
+        }
+
+        vector<vector<int>> result;
+        while (!maxHeap.empty()) {
+            result.push_back(maxHeap.top().second);
+            maxHeap.pop();
+        }
+
+        return result;
+    }
+};
+```
